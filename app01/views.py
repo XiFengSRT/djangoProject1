ed873c5b97757ba0e8c021644d707a603ed2bbe6
XiFengSRT <1225041397@qq.com>
2022-05-12 08:21:28 +0800

    这是我第一次提交文件

--- app01/views.py
+++ app01/views.py
@@ -1,8 +1,15 @@
 from django.shortcuts import render, HttpResponse
-
+import requests as req
 
 # Create your views here.
 def index(request):
     return HttpResponse("欢迎使用")
+
+
 def use_html(request):
-    return render(request,"use_html.html")
+    # http://www.chinaunicom.com.cn/api/article/NewsByIndex/2/2022/05/news
+        # 利用python获取数据，需要使用第三方库 requests
+    results = req.get("http://www.chinaunicom.com.cn/api/article/NewsByIndex/2/2022/05/news")
+    date_list = results.json()
+    print(date_list)
+    return render(request, "use_html.html")
